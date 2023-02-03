3b3f972fe824b3f0111d455049be94b292f969be
Ickarus75 <ickarus75@hotmail.com>
2023-02-03 16:07:12 -0600

    added fetch_user_id function

--- github_interface/batch_add_user_test.py
+++ github_interface/batch_add_user_test.py
@@ -11,19 +11,33 @@ headers = {"Authorization": f"Token {auth_token}"}
 
 # Set the organization name and the GitHub username of the user to invite
 org_name = "tangoplatoon"
-user_id = 17520323  # id number for my user; Got it through calling GET https://api.github.com/users/zalmoujahed
+user_id = 1092258960  # id number for my user; Got it through calling GET https://api.github.com/users/zalmoujahed
 
 # Set the API endpoint for sending an invitation
 url = f"https://api.github.com/orgs/{org_name}/invitations"
 
-# Set the data for the API call
+# Set the data for the POST API call
 data = {"invitee_id": user_id}
 
 # Make the POST request to send the invitation
-response = requests.post(url, json=data, headers=headers)
+# response = requests.post(url, json=data, headers=headers)
 
 # Check the status code of the response
-if response.status_code == 201:
-    print("Invitation sent successfully.")
-else:
-    print("An error occurred: ", response.json())
+# if response.status_code == 201:
+#     print("Invitation sent successfully.")
+# else:
+#     print("An error occurred: ", response.json())
+
+
+def fetch_user_id(github_username, headers):
+    url = f"https://api.github.com/search/users?q={github_username}"
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # print(data['items'][0]['id']) 
+    return data['items'][0]['id']  # RETURNS the ID of the user
+
+print(fetch_user_id("Ickarus75", headers))
+
+    
