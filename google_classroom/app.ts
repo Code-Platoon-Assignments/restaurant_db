5cf43c5cbcb6d5db85b33140056e2b23cde1016f
Adam Cahan <adam.cahan@gmail.com>
2023-06-03 15:15:11 -0500

    google_classroom get course by id

--- google_classroom/app.ts
+++ google_classroom/app.ts
@@ -1,4 +1,6 @@
 /**
+ * Run this to look at the JSON object w/ids, names, calendar ids, etc, for each course.
+ * You can also use typescript type hints.
  * Lists 10 course names and ids.
  * This is here for "hello world" & testing purposes to confirm everything works.
  */
@@ -10,6 +12,7 @@ function listCourses() {
     pageSize: 10
     // Use other parameter here if needed
   };
+
   try {
     // call courses.list() method to list the courses in classroom
     const response = Classroom.Courses.list(optionalArgs);
@@ -28,15 +31,30 @@ function listCourses() {
     // get errors like PERMISSION_DENIED/INVALID_ARGUMENT/NOT_FOUND
     console.log('Failed with error %s', err.message);
   }
-}  
+}
+
+function getCourse(courseId: string) {
+  const course = Classroom.Courses.get(courseId);
+  console.log(`Got course - ${course.descriptionHeading} - id - ${course.id}`);
+  return course;
+}
+
+/** Helper functions */
+function getTangoPlatoon() {
+  return getCourse('576555342077');
+}
+
+function getUniformPlatoon() {
+  return getCourse('612339173048');
+}
 
 /***
  * hello world functions you can run to make sure everything works.
  */
 const helloWorld = (person: string) => {
-    return `Hello, ${person}!`;
-  }
-  
+  return `Hello, ${person}!`;
+}
+
 function testHelloWorld() {
   const user = 'Grant';
   Logger.log(helloWorld(user));
